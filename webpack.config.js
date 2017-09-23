a304f4bf832312916932ad2572c4c1ef2812b5d0
Tyler Eakes <teakesprofessional@gmail.com>
2017-09-23 14:33:17 -0700

    SCSS loader added

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
 
 module.exports = {
   entry: './client/index.js',
@@ -16,8 +16,11 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: "style-loader"},
+      { test: /\.scss$/, loader: "css-loader"},
+      { test: /\.scss$/, loader: "sass-loader"},
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
-}
+};
