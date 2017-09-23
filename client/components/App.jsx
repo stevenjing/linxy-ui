ab69e94587c4689ef6271bd641403f1f9208930a
Marvin Corro <corro.marvin@gmail.com>
2017-09-23 15:18:12 -0700

    styles are now supported

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {Grid, Row, Col, Jumbotron} from 'react-bootstrap';
+import "../styles.scss"
 
 export default class App extends React.Component {
   render() {
---
+++ client/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {Grid, Row, Col, Jumbotron} from 'react-bootstrap';
+import "../styles.scss"
+
+export default class App extends React.Component {
+
+    render()
+    {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
