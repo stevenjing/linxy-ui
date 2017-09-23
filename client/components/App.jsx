572c73fc44a6c36f1b014f408c2df50fbfa4feb9
Marvin Corro <corro.marvin@gmail.com>
2017-09-23 14:43:59 -0700

    added bootstrap support

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { render } from 'react-dom';
+import {Grid, Row, Col, Jumbotron} from 'react-bootstrap';
 
 export default class App extends React.Component {
   render() {
     return (
      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+        <Grid>
+            <Row>
+                <Col sm={12} md={12} lg={12}>
+                    <Jumbotron>
+                        <h1>Bootstrap test</h1>
+                    </Jumbotron>
+                </Col>
+            </Row>
+        </Grid>
+    </div>);
   }
 }
